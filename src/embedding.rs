875c901fec92c3f3cce092f7c845ac57ce9d19f4
Michael Anderson <drmikeando@gmail.com>
2024-05-04 07:20:01 +0800

    Better error handling in the embedding code

--- src/embedding.rs
+++ src/embedding.rs
@@ -1,7 +1,20 @@
 use reqwest::Client;
 use serde_json::json;
+use snafu::{OptionExt, ResultExt, Snafu};
 
 use crate::json::ToJson;
+
+#[derive(Debug, Snafu)]
+pub enum Error {
+    #[snafu(display("Could not make http request to OpenAI"))]
+    Request { source: reqwest::Error },
+
+    #[snafu(display("Bad HTTP response status from OpenAI ({status})"))]
+    Http { response_text: String, status: u16 },
+
+    #[snafu(display("Returned JSON was not of the expected from: {reason}"))]
+    Structure { reason: String },
+}
 
 struct EmbeddingRequest {
     input: String,
@@ -18,7 +31,10 @@ impl ToJson for EmbeddingRequest {
 }
 
 //TODO: Add a caching layer like we use for completion requests.
-pub async fn make_uncached_embedding_request(text: &str, openai_api_key: &str) -> Vec<f32> {
+pub async fn make_uncached_embedding_request(
+    text: &str,
+    openai_api_key: &str,
+) -> Result<Vec<f32>, Error> {
     let client = Client::new();
 
     let request = EmbeddingRequest {
@@ -33,16 +49,36 @@ pub async fn make_uncached_embedding_request(text: &str, openai_api_key: &str) -
         .json(&request.to_json())
         .send()
         .await
-        .unwrap();
+        .context(RequestSnafu)?;
+
+    let response_status = response.status();
+    if !response_status.is_success() {
+        eprintln!("Error performing request!");
+        let response_text = response.text().await.unwrap();
+        eprintln!("---- raw response ---\n{}\n", response_text);
+        return HttpSnafu {
+            response_text,
+            status: response_status.as_u16(),
+        }
+        .fail();
+    }
 
     let response_text = response.text().await.unwrap();
-    // eprintln!("---- raw response ---\n{}\n", response_text);
+    eprintln!("---- raw response ---\n{}\n", response_text);
     let v: serde_json::Value = serde_json::from_str(&response_text).unwrap();
-    let r: Vec<f32> = v["data"][0]["embedding"]
+
+    let values = v["data"][0]["embedding"]
         .as_array()
-        .unwrap()
+        .with_context(|| StructureSnafu {
+            reason: "no data[0].embedding entry",
+        })?;
+
+    let r: Option<Vec<f32>> = values
         .iter()
-        .map(|v| v.as_f64().unwrap() as f32)
+        .map(|v| v.as_f64().map(|v| v as f32))
         .collect();
-    r
+    let r = r.with_context(|| StructureSnafu {
+        reason: "no data[0].embedding contained non-numeric values",
+    })?;
+    Ok(r)
 }
